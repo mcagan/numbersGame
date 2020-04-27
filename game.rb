c697b95bb37b07e2560cb73167eb12f58187c57a
mcagan <meliscagan@gmail.com>
2020-04-27 19:07:01 +0000

    implement game

---
+++ game.rb
@@ -0,0 +1,65 @@
+require_relative './players'
+
+class Game
+  def initialize
+    @player1 = Player.new("Player 1")
+    @player2 = Player.new("Player 2")
+    @players = [@player1, @player2]
+    @round = 1
+  end
+
+  def display_round_status
+    puts "---------------"
+    puts "Round #{@round}"
+    puts "---------------"
+    @players.each{|player| puts "#{player.name} has #{player.lives} lives"}
+  end
+
+  def display_winner
+    winner = @players.select{|player| player.lives > 0}
+    puts "The winner is #{winner.name}"
+  end
+
+   def game_over?
+    if @players.first.lives == 0 || @players.last.lives === 0
+      true
+    else
+      false
+    end
+  end
+
+  def next_round
+    @round += 1
+    sleep 1
+    @players.rotate!
+  end
+
+  def ask_question
+    num1 = rand(1...20)
+    num2 = rand(1...20)
+    result = num1 + num2
+    puts "What is #{num1} + #{num2}?"
+    answer = gets.chomp
+    if answer.to_i == result
+      puts "That's correct!"
+      true
+    else
+      puts "That's wrong! You lose a life."
+      @players.first.lives -= 1
+      false
+    end
+  end
+
+  def play
+    until game_over? do
+      current_player = @players.first
+      display_round_status
+      ask_question
+      next_round
+    end
+    display_winner
+  end
+end
+
+
+
---
+++ players.rb
@@ -0,0 +1,13 @@
+class Player
+  attr_accessor :lives
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  def dead?
+    @lives <= 0
+  end
+end
